ea1ab02904628433a90113ffd8bea6f91726bb0b
dr.dimitru <ceo@veliov.com>
2015-02-09 21:31:34 +0300

    Upd deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:mailer',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Easy peasy email sending within HTML-Templates via SMTP [SERVER ONLY]',
   git: 'https://github.com/VeliovGroup/Meteor-Mailer',
   documentation: 'README.md'
@@ -9,5 +9,5 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
   api.addFiles('ostrio:mailer.coffee', 'server');
-  api.use(['coffeescript', 'templating', 'email', 'spacebars', 'meteorhacks:ssr@2.1.1', 'underscore', 'ostrio:jsextensions@0.0.2'], 'server');
+  api.use(['coffeescript', 'templating', 'email', 'spacebars', 'meteorhacks:ssr@2.1.1', 'underscore', 'ostrio:jsextensions@0.0.4'], 'server');
 });
