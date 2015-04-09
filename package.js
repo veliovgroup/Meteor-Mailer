ee906a7805bc24adcd373869974f5bc04bdc09e5
dr.dimitru <ceo@veliov.com>
2015-04-09 19:42:42 +0300

    v0.2.1
     - Deps upd

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'ostrio:mailer',
-  version: '0.2.0',
+  version: '0.2.1',
   summary: 'Easy peasy put emails into queue to send them within HTML-Templates via SMTP [SERVER ONLY]',
   git: 'https://github.com/VeliovGroup/Meteor-Mailer',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+  api.versionsFrom('1.0');
   api.addFiles('ostrio:mailer.coffee', 'server');
   api.use(['sha', 'coffeescript', 'templating', 'email', 'spacebars', 'meteorhacks:ssr@2.1.1', 'underscore', 'ostrio:jsextensions@0.0.4'], 'server');
 });
